cfd5440dbb8b1fad2c6e67f005e105b0cd3c1aba
LuisMoraEL <luismora045@outlook.com>
2024-10-28 15:29:23 -0600

    feat(esp32 BT): version previa del control del esp32 y corrección de errores, no acabado

--- App-RK-BM/Controller/Activities/ActivitiesViewController.swift
+++ App-RK-BM/Controller/Activities/ActivitiesViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ActivitiesViewController: UIViewController {
-
+class ActivitiesViewController: UIViewController, BluetoothManagerDelegate {
+    
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
+    @IBOutlet weak var connectButton: UIButton!
+    @IBOutlet weak var endSensingButton: UIButton!
+    @IBOutlet weak var startSensingButton: UIButton!
+    @IBOutlet weak var disconnectButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         title = "Actividades"
+        
+        BluetoothManager.shared.delegate = self
     }
-
+    
+    @IBAction func connectButtonTapped(_ sender: UIButton) {
+        BluetoothManager.shared.startScanning()
+    }
+    
+    @IBAction func disconnectButtonTapped(_ sender: UIButton) {
+        BluetoothManager.shared.disconnect()
+    }
+    
+    @IBAction func startSensingButtonTapped(_ sender: UIButton) {
+        BluetoothManager.shared.sendCommand("start_sensing")
+    }
+    @IBAction func endSensingButtonTapped(_ sender: UIButton) {
+        BluetoothManager.shared.sendCommand("end_sensing")
+    }
+    
+    // BluetoothManagerDelegate - Actualizar estado de conexión
+    func didUpdateConnectionStatus(isConnected: Bool) {
+        let status = isConnected ? "Conectado": "Desconectado"
+        print("Estado de conexión: \(status)")
+        self.stateLabel.text = "Estado: " + status
+    }
+    
+    // BluetoothManagerDelegate - Datos recibidos
+    func didReceiveData(_ data: String) {
+        print("Datos recibidos: \(data)")
+        self.dataLabel.text = "Datos recibidos: " + data
+    }
 }
